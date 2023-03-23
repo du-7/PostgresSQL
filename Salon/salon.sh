07b4f36385200d0b894680fdec4c5b3b8a0ed611
Ricardo Custódio <92888158+du-7@users.noreply.github.com>
2023-03-23 17:18:02 +0000

    Add files via upload

--- Salon/salon.sh
+++ Salon/salon.sh
@@ -1,1 +1,58 @@
+#! /bin/bash
 
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
+
+echo -e "\n~~~~~ Salon Menu ~~~~~\n"
+
+MAIN_MENU(){
+  # get available services
+  AVAILABLE_SERVICES=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
+
+  if [[ $AVAILABLE_SERVICES ]]
+  then
+    echo "$AVAILABLE_SERVICES" | while read SERVICE NAME
+    do
+      echo "$SERVICE) $NAME" | sed 's/ | / /'
+    done
+  fi
+
+  read SERVICE_ID_SELECTED
+
+  # get if service exist
+  SERVICE_ID=$($PSQL "SELECT service_id FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+
+  # if no service available
+  if [[ -z $SERVICE_ID ]]
+  then
+    echo -e ""
+    MAIN_MENU
+  else
+    echo -e "\nWhat's your phone number?"
+    read CUSTOMER_PHONE
+
+    CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+    # if customer doesn't exist
+    if [[ -z $CUSTOMER_NAME ]]
+    then
+      # get new customer name
+      echo -e "\nI don't have a record for that phone number, what's your name?"
+      read CUSTOMER_NAME
+
+      # insert new customer
+      INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')") 
+    fi
+
+    SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED")
+
+    echo -e "\nWhat time would you like your $(echo $SERVICE_NAME | sed -r 's/^ *| *$//g') , $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g').?"
+    read SERVICE_TIME
+
+    CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE'")
+    
+    INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES('$CUSTOMER_ID', '$SERVICE_ID_SELECTED', '$SERVICE_TIME')") 
+    echo -e "\nI have put you down for a $(echo $SERVICE_NAME | sed -r 's/^ *| *$//g') at $SERVICE_TIME, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
+    
+  fi
+}
+
+MAIN_MENU
